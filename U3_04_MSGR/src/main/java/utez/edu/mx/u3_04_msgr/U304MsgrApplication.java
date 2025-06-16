66b85bb1f7330608a19a2f553c887372ab188dfd
Moisantiagogr <20223tn055@utez.edu.mx>
2025-06-16 15:52:09 -0600

    [FIRST COMMIT] se subio la esructura del proyecto

---
+++ U3_04_MSGR/src/main/java/utez/edu/mx/u3_04_msgr/U304MsgrApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.u3_04_msgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U304MsgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U304MsgrApplication.class, args);
+	}
+
+}
---
+++ U3_04_MSGR/src/test/java/utez/edu/mx/u3_04_msgr/U304MsgrApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.u3_04_msgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U304MsgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
